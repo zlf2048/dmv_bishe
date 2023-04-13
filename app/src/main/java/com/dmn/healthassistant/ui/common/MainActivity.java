e598892c1c745dc6fdb93921f4d1053d7fe65308
zlf2048 <zlf2048@qq.com>
2023-04-13 23:41:40 +0800

    场面有了

--- app/src/main/java/com/dmn/healthassistant/ui/common/MainActivity.java
+++ app/src/main/java/com/dmn/healthassistant/ui/common/MainActivity.java
@@ -56,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//        fragmentTransaction.add(R.id.view,fragment);
         fragmentTransaction.replace(R.id.view, fragment);
         fragmentTransaction.commit();
     }
