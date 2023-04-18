9ea6a75e196a775ae7d8b3b258d08d335c960fcc
xxy <1452769581@qq.com>
2023-04-18 11:57:21 +0800

    dmv

--- app/src/main/java/com/dmn/healthassistant/ui/individuality/IndividualityBottomFragment.java
+++ app/src/main/java/com/dmn/healthassistant/ui/individuality/IndividualityBottomFragment.java
@@ -6,10 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import com.dmn.healthassistant.R;
-import com.dmn.healthassistant.ui.individuality.personal.PersonalInformationActivity;
+import com.dmn.healthassistant.ui.individuality.personal.BasicInformationActivity;
+import com.dmn.healthassistant.ui.individuality.personal.HealthyInformationActivity;
 import com.dmn.healthassistant.ui.individuality.setting.SettingActivity;
 import com.google.android.material.button.MaterialButton;
 //import com.dmn.healthassistant.ui.individuality.settings.SettingActivity;
@@ -20,13 +20,15 @@ import com.google.android.material.button.MaterialButton;
 public class IndividualityBottomFragment extends Fragment {
 
     private MaterialButton settings;
-    private MaterialButton personal_information;
+    private MaterialButton healthy_information;
+    private MaterialButton basic_information;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_individuality_bottom, container, false);
         settings = view.findViewById(R.id.settings);
-        personal_information = view.findViewById(R.id.personal_information);
+        healthy_information = view.findViewById(R.id.healthy_information);
+        basic_information = view.findViewById(R.id.basic_information);
         return view;
     }
 
@@ -40,10 +42,19 @@ public class IndividualityBottomFragment extends Fragment {
 //                startActivity(intent);
 //            }
 //        });
-        personal_information.setOnClickListener(new View.OnClickListener() {
+
+        basic_information.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), PersonalInformationActivity.class);
+                Intent intent = new Intent(getActivity(), BasicInformationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        healthy_information.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), HealthyInformationActivity.class);
                 startActivity(intent);
             }
         });
