74095bc7fa50735659ccc078f12ced9ea9f4efb6
zlf2048 <zlf2048@qq.com>
2023-04-22 15:56:04 +0800

    完善了日期选择器

--- app/src/main/java/com/dmn/healthassistant/ui/individuality/personal/HealthyInformationActivity.java
+++ app/src/main/java/com/dmn/healthassistant/ui/individuality/personal/HealthyInformationActivity.java
@@ -2,15 +2,39 @@ package com.dmn.healthassistant.ui.individuality.personal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.DatePicker;
 
 import com.dmn.healthassistant.R;
+import com.google.android.material.textfield.TextInputEditText;
 
-public class HealthyInformationActivity extends AppCompatActivity {
+import java.util.Calendar;
 
+public class HealthyInformationActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    private TextInputEditText name, birth_data;
+
+    @Override
+    //日期选择完事件
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String desc = String.format("%d-%d-%d",year,month+1,dayOfMonth);
+        birth_data.setText(desc);//设置生日
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_healthy_information);
+
+        birth_data = findViewById(R.id.birth_data);
+
+        birth_data.setOnClickListener(v -> {//生日点击弹出日期选择框
+            Calendar calendar = Calendar.getInstance();//获取Calendar实例
+            //创建日期选择器
+            DatePickerDialog dialog = new DatePickerDialog(this,this,
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MARCH),
+                    calendar.get(Calendar.DAY_OF_MONTH));
+            dialog.show();//窗口弹出
+        });
     }
 }
