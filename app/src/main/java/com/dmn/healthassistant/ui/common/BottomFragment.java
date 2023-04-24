09a64e73a09f20a5e42461612db26543e9d7fa57
zlf2048 <zlf2048@qq.com>
2023-04-24 12:30:45 +0800

    人头修复

--- app/src/main/java/com/dmn/healthassistant/ui/common/BottomFragment.java
+++ app/src/main/java/com/dmn/healthassistant/ui/common/BottomFragment.java
@@ -85,7 +85,7 @@ public class BottomFragment extends Fragment {
         diet.setTextColor(Color.BLACK);
         diet.setCompoundDrawablesRelativeWithIntrinsicBounds(null, ContextCompat.getDrawable(requireContext(), R.drawable.diet), null ,null);
         individuality.setTextColor(Color.BLACK);
-        individuality.setCompoundDrawablesRelativeWithIntrinsicBounds(null, ContextCompat.getDrawable(requireContext(), R.drawable.individuality), null ,null);
+        individuality.setCompoundDrawablesRelativeWithIntrinsicBounds(null, ContextCompat.getDrawable(requireContext(), R.drawable.mine), null ,null);
 
         // 更改被点击按钮的文本颜色和图片
         clickedButton.setTextColor(Color.parseColor("#0cb6ce"));
