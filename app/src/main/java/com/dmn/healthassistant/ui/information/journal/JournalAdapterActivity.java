3f7c81b6787a6fa0a0284dd0103f5903bdc593c3
xxy <1452769581@qq.com>
2023-05-08 22:06:28 +0800

    dsg

--- app/src/main/java/com/dmn/healthassistant/ui/information/journal/JournalAdapterActivity.java
+++ app/src/main/java/com/dmn/healthassistant/ui/information/journal/JournalAdapterActivity.java
@@ -3,6 +3,8 @@ package com.dmn.healthassistant.ui.information.journal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,9 +16,21 @@ import com.dmn.healthassistant.ui.information.NewsDetailActivity;
 import com.dmn.healthassistant.ui.information.adapter.MyAdapter;
 import com.dmn.healthassistant.ui.information.article.ArticleAdapterActivity;
 import com.dmn.healthassistant.ui.information.bean.ItemBean;
+import com.minapp.android.sdk.database.Record;
+import com.minapp.android.sdk.database.Table;
+import com.minapp.android.sdk.database.query.Query;
+import com.minapp.android.sdk.util.PagedList;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class JournalAdapterActivity extends AppCompatActivity {
 
@@ -36,37 +50,98 @@ public class JournalAdapterActivity extends AppCompatActivity {
 
     private void initEvent(){
         mMyAdapter = new MyAdapter(this,mBeanList);
+        System.out.println(mListView);
         mListView.setAdapter(mMyAdapter);
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 ItemBean itemBean = mBeanList.get(i);
-                String title = itemBean.getTitle();
-                Intent intent = new Intent(JournalAdapterActivity.this, NewsDetailActivity.class);
-                intent.putExtra("item",itemBean);
-                startActivity(intent);
 
-                Toast.makeText(JournalAdapterActivity.this, "你点击了"+i+title, Toast.LENGTH_SHORT).show();
-            }
-        });
-        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                return false;
+                Intent intent = new Intent(JournalAdapterActivity.this, NewsDetailActivity.class);
+                intent.putExtra("id", itemBean.getId());
+                intent.putExtra("type", "journal");
+                startActivity(intent);
             }
         });
     }
 
     private void initData(){
+        Table journal = new Table("journal");
+        Record[] record = new Record[20];
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Query query = new Query().offset(0).limit(20);
+                    PagedList<Record> records = journal.query(query);
+                    List<Record> list = records.getObjects();
+
+                    int size = list.size();
+                    for (int i = 0; i < size; i++) {
+                        record[i] = list.get(i);
+                    }
+
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+        });
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        int threadPoolSize = 5; // 线程池大小
+        ExecutorService executor = Executors.newFixedThreadPool(threadPoolSize); // 创建一个固定大小的线程池
+        Bitmap[] bitmaps = new Bitmap[20];
+        for (int i = 0; i < record.length && record[i] != null; i++) {
+            final int[] index = new int[]{i};
+            Record record1 = record[i];
+            executor.execute(new Runnable() { // 提交任务给线程池执行
+                @Override
+                public void run() {
+                    Bitmap bitmap = null;
+                    try {
+                        URL url = new URL(record1.getString("img"));
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                        connection.setDoInput(true);
+                        connection.connect();
+                        InputStream input = connection.getInputStream();
+                        bitmap = BitmapFactory.decodeStream(input);
+                    } catch (MalformedURLException e) {
+                        throw new RuntimeException(e);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    bitmaps[index[0]] = bitmap;
+                    System.out.println(bitmaps[index[0]]);
+                }
+            });
+        }
+        executor.shutdown(); // 关闭线程池
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            // 处理异常
+        }
+
         mBeanList = new ArrayList<>();
 
-        ItemBean newsBean1 = new ItemBean();
-        newsBean1.setTitle("雨中漫步");
-        newsBean1.setContent("人生到处知何似，应似飞鸿踏雪泥。人的一生，辗转各处，像什么呢？正像到处飞的鸟类。到处飞是鸟的命运，是必然，偶尔在雪地上留下脚印，是偶然。");
-//        newsBean1.setImgResId(R.drawable.test1);
+        for (int i = 0; i < record.length && record[i] != null; i++) {
+            Record record1 = record[i];
 
+            ItemBean newsBean = new ItemBean();
+            newsBean.setId(record1.getId());
+            newsBean.setTitle(record1.getString("title"));
+            newsBean.setContent(record1.getString("abstract"));
+            newsBean.setImgBitmap(bitmaps[i]);
 
-        mBeanList.add(newsBean1);
+            mBeanList.add(newsBean);
+        }
     }
 
     private void initView(){
